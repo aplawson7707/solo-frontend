73086a9354730b4a5c2c12884a70851d9c279c9a
aplawson7707 <aplawson7707@gmail.com>
2022-03-01 00:20:34 -0800

    Add beginning of simple footer and update home page.

---
+++ components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+    <footer class="footer">
+        <div class="footer-notes">
+            <div class="col" style="float: left; width: 60%">
+                <div style="margin-bottom: 20px">
+                Built with 
+                <a href="https://github.com/nuxt/nuxt.js" target="_blank">
+                    Nuxt.js
+                </a>
+                </div>
+                <div style="margin-bottom: 20px">
+                    Powered by 
+                    <a href="https://www.djangoproject.com" target="_blank">
+                        <span class="powered-by">Django</span>
+                    </a>
+                </div>
+            </div>
+            <div class="col" style="float: left; width: 40%">
+                <div style="margin-bottom: 20px">
+                    &copy; {{ new Date().getFullYear() }} Alex Lawson
+                </div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </footer>
+</template>
+
+<style>
+    .footer {
+        background-color: #343a40;
+        width: 100%;
+    }
+    .footer-notes {
+        margin: 0 auto;
+        max-width: 960px;
+        padding: 15px;
+        padding-bottom: 60px;
+        padding-top: 40px;
+    }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,5 +2,6 @@
   <div>
     <TopNav />
     <Nuxt />
+    <Footer />
   </div>
 </template>
