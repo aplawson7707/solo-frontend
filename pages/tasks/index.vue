2051877de89d8e5e515c30d3187caad63ed7f682
aplawson7707 <aplawson7707@gmail.com>
2022-03-07 00:51:14 -0800

    Add basic task page using new api.

--- pages/tasks/index.vue
+++ pages/tasks/index.vue
@@ -1,14 +1,69 @@
 <template>
     <b-container>
-        <div>
-            <h1>This is the Tasks page</h1>
-        </div>
         <b-row>
+            <b-col cols="3">
+                <b-calendar class="mt-4">
+                </b-calendar>
+            </b-col>
             <b-col>
-                <b-card class="mt-4">
-                    <h4>Task Name</h4>
+                <b-card title="My Tasks" class="mt-4">
+                    <b-table
+                        id="tasksTable"
+                        ref="tasksTable"
+                        :items="getTasks"
+                        :fields="fields"
+                        striped
+                        sticky-header="true"
+                        head-variant="dark"
+                        hover
+                    >
+                        <template #cell(completed)="row">
+                            <b-icon-check-square
+                                v-if="row.item.completed > 0"
+                                variant="success"
+                                class="align-middle"
+                            />
+                        </template>
+                    </b-table>
                 </b-card>
             </b-col>
         </b-row>
     </b-container>
 </template>
+
+<script>
+import {
+    BIconCheckSquare,
+} from 'bootstrap-vue';
+
+export default {
+    components: {
+        BIconCheckSquare,
+    },
+    data() {
+        return {
+            fields: [
+                'title',
+                'description',
+                'date',
+                'completed',
+            ],
+        }
+    },
+    methods: {
+        getTasks() {
+            return this.$axios.get(`/api/tasks/`).then((resp) => {
+                this.taskList = resp.data.map((item) => {
+                    return {
+                        title: item.title,
+                        description: item.description,
+                        completed: item.completed,
+                        date: item.created_at,
+                    }
+                })
+                return this.taskList || []
+            })
+        },
+    },
+}
+</script>
