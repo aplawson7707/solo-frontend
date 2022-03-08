679abbc6be8db9650e86820094457bd175d9ad06
aplawson7707 <aplawson7707@gmail.com>
2022-03-07 21:32:09 -0800

    Commit before add_tailwind

--- pages/tasks/index.vue
+++ pages/tasks/index.vue
@@ -1,54 +1,54 @@
 <template>
     <b-container>
-        <b-row>
-            <b-col cols="4">
-                <b-card class="mt-4 text-center">
-                    <b-calendar v-model="value" value-as-date locale="en">
-                        <div class="d-flex" dir="ltr">
-                            <b-button
-                                size="sm"
-                                variant="outline-danger"
-                                v-if="value"
-                                @click="clearDate"
-                            >
-                                Clear Date
-                            </b-button>
-                            
-                            <b-button
-                                size="sm"
-                                variant="outline-primary"
-                                class="ml-auto"
-                                @click="setToday"
-                            >
-                                Set Today
-                            </b-button>
-                        </div>
-                    </b-calendar>
-                </b-card>
-            </b-col>
-            <b-col>
-                <b-card title="My Tasks" class="mt-4">
-                    <b-table
-                        id="tasksTable"
-                        ref="tasksTable"
-                        class="text-center"
-                        head-variant="dark"
-                        sticky-header="true"
-                        :items="getTasks"
-                        :fields="fields"
-                        striped
-                        hover
-                    >
-                        <template #cell(completed)="row">
-                            <b-icon-check-square
-                                v-if="row.item.completed > 0"
-                                variant="success"
-                            />
-                        </template>
-                    </b-table>
-                </b-card>
-            </b-col>
-        </b-row>
+            <b-row>
+                <b-col cols="4">
+                    <b-card class="mt-4 text-center">
+                        <b-calendar v-model="value" value-as-date locale="en">
+                            <div class="d-flex" dir="ltr">
+                                <b-button
+                                    size="sm"
+                                    variant="outline-danger"
+                                    v-if="value"
+                                    @click="clearDate"
+                                >
+                                    Clear Date
+                                </b-button>
+                                
+                                <b-button
+                                    size="sm"
+                                    variant="outline-primary"
+                                    class="ml-auto"
+                                    @click="setToday"
+                                >
+                                    Set Today
+                                </b-button>
+                            </div>
+                        </b-calendar>
+                    </b-card>
+                </b-col>
+                <b-col>
+                    <b-card title="My Tasks" class="mt-4">
+                        <b-table
+                            id="tasksTable"
+                            ref="tasksTable"
+                            class="text-center"
+                            head-variant="dark"
+                            sticky-header="true"
+                            :items="getTasks"
+                            :fields="fields"
+                            striped
+                            hover
+                        >
+                            <template #cell(completed)="row">
+                                <b-icon-check-square
+                                    v-if="row.item.completed > 0"
+                                    variant="success"
+                                />
+                            </template>
+                        </b-table>
+                    </b-card>
+                </b-col>
+            </b-row>
     </b-container>
 </template>
 
